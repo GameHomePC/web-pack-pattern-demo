2617b7700e0b3b20a20442b90131d26b5cdc1fb8
Yauhen Aliakseyenko <yauheni.aliakseyenko@neklo.com>
2016-03-17 18:51:31 +0300

    update

---
+++ public/about.js
@@ -0,0 +1,23 @@
+var about =
+webpackJsonp_name_([0],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var _welcome = __webpack_require__(1);
+
+	var _welcome2 = _interopRequireDefault(_welcome);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	(0, _welcome2.default)("home");
+
+	console.log(("development") == 'development');
+	console.log(("EVGENYALEXSEENK"));
+
+	exports.welcome = _welcome2.default;
+
+/***/ }
+]);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYWJvdXQuanMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vZnJvbnRlbmQvYWJvdXQuanMiXSwic291cmNlc0NvbnRlbnQiOlsiJ3VzZSBzdHJpY3QnO1xyXG5cclxuaW1wb3J0IHdlbGNvbWUgZnJvbSAnLi93ZWxjb21lJztcclxuXHJcbndlbGNvbWUoXCJob21lXCIpO1xyXG5cclxuY29uc29sZS5sb2coTk9ERV9FTlYgPT0gJ2RldmVsb3BtZW50Jyk7XHJcbmNvbnNvbGUubG9nKFVTRVJET01BSU4pO1xyXG5cclxuZXhwb3J0cy53ZWxjb21lID0gd2VsY29tZTtcblxuXG4vKiogV0VCUEFDSyBGT09URVIgKipcbiAqKiBmcm9udGVuZC9hYm91dC5qc1xuICoqLyJdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQTtBQUNBO0FBQ0E7QUFDQTs7Ozs7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBOzs7OyIsInNvdXJjZVJvb3QiOiIifQ==
